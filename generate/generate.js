3bcc6da9b1c3dc3f81b0c7f2d56d133617e46e7e
Israel Blancas <iblancasa@gmail.com>
2015-07-20 20:34:03 +0200

    Changing generate methods

---
+++ generate/generate.js
@@ -0,0 +1,20 @@
+var fs = require('fs'),
+    ejs = require("ejs");
+
+
+function ejs2html(name, information) {
+  str = fs.readFileSync("./sections/"+name+".ejs", 'utf8');
+  information['filename'] = "./sections/"+name+".ejs";
+  ret = ejs.render(str,information);
+
+  fs.writeFile("../"+name+".html", ret, function (err) {
+    if (err) return console.log(err);
+    console.log(name + " generated");
+  });
+}
+
+files = {};
+
+files['index'] = {title: "Home - GitHub Processed Data Ranking"};
+
+ejs2html("index",files['index']);
